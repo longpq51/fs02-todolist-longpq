70617dcd28b4961a67a7121fc469d6514a5cf761
Pham Quang Long <phamquanglong51@gmail.com>
2022-08-18 23:28:06 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "@reduxjs/toolkit";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import rootReducer from "./store/reducer";
+// import store from "./store/store";
+const store = createStore(rootReducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
